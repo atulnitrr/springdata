5a579cc27bacd3f3341bdc81b2046655893073cf
Atul Kumar <atulkumar458@gmail.com>
2020-04-08 16:05:47 +0530

    One to one mapping

---
+++ src/main/java/com/atul/springdata/springdata/entity/Notes.java
@@ -0,0 +1,30 @@
+package com.atul.springdata.springdata.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+@Getter
+@Setter
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String note;
+
+
+}
+
---
+++ src/main/java/com/atul/springdata/springdata/entity/Recipe.java
@@ -0,0 +1,37 @@
+package com.atul.springdata.springdata.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Getter
+@Setter
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer serving;
+    private String source;
+    private String url;
+    private String direction;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+}
