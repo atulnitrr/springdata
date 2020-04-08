1f816e4c2a7baf738ee97ca0fd5d53e222789d6b
Atul Kumar <atulkumar458@gmail.com>
2020-04-08 14:53:24 +0530

    Initial commit

---
+++ src/main/java/com/atul/springdata/springdata/SpringdataApplication.java
@@ -0,0 +1,14 @@
+package com.atul.springdata.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataApplication.class, args);
+		System.out.println("Spring Data App is up and running -->");
+	}
+
+}
